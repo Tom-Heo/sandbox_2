6c053e8c68cacc976831db39e79bb1ca8eaf55c1
tomheo <tom@generativelab.co.kr>
2026-02-28 02:12:43 +0900

    1.4.0

--- core/block.py
+++ core/block.py
@@ -30,17 +30,19 @@ class Block(nn.Module):
         super().__init__()
         self.dim = dim
 
-        self.conv001 = nn.Conv2d(dim, dim * 3, 1, 1, 0)
+        self.conv0001 = nn.Conv2d(dim, dim * 3, 1, 1, 0)
+        self.conv001 = nn.Conv2d(dim * 3, dim * 3, 1, 1, 0)
+        self.act1 = Heo.HeLU2d(dim * 3)
         self.simple_gate = SimpleGate()
-        self.act1 = Heo.HeLU2d(dim)
         self.conv01 = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
         self.residual_gate = Heo.HeoGate2d(dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         raw = x
+        x = self.conv0001(x)
         x = self.conv001(x)
-        x = self.simple_gate(x)
         x = self.act1(x)
+        x = self.simple_gate(x)
         x = self.conv01(x)
         x = self.residual_gate(x, raw)
         return x
